7c293ef612951f2f0c235b510f71b8b11275719f
Dario Karanovi <kleetsonfire@gmail.com>
2021-04-12 11:08:19 -0400

    desfrg

--- main.js
+++ main.js
@@ -3,20 +3,20 @@
 /////////////////////////////////////////////////////
 
 function compoundInterest() {
-    let p = parseFloat(document.getElementById('pVal').value);
-    let i = parseFloat(document.getElementById('iRV').value);
-    let time = parseFloat(document.getElementById('timeVal').value);
-    let n = parseFloat(document.getElementById('nVal').value);
-    let total = p*(1 + i/n)**(n*time);
+    var p = (document.getElementById('pVal').value);
+    var i = (document.getElementById('iRV').value);
+    var time = (document.getElementById('timeVal').value);
+    var n = (document.getElementById('nVal').value);
+    var total = p*((1 + i/n)**(n*time));
     
-    let msg = "the total is " + String(total);
-    
+    var msg = "the total is " + String(total);
+    var pos = "the values must be positive"
     
     if (p || i || time || n >= 0) {
         document.getElementById('interest').innerHTML = msg;
     }
     else {
-        document.getElementById('interest').innerHTML = "the values must be positive";
+        document.getElementById('interest').innerHTML = pos;
     }
 }
 
